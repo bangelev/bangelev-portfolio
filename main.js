eb21219596c21572eff7de09eef1253131f7ff75
bangelev <bangelevski83@gmail.com>
2022-06-23 09:29:12 +0200

    Initia commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll('.section')
+const secBtns = document.querySelectorAll('.controls')
+const secBtn = document.querySelectorAll('.control')
+const allSections = document.querySelectorAll('.main-content')
+
+const themeBtn = document.querySelector('.theme-btn')
+
+secBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        document.querySelector('.active-btn').classList.remove('active-btn')
+        document.querySelector('.active').classList.remove('active')
+        e.target.classList.add('active-btn')
+        let secElement = document.querySelector(`#${e.target.dataset.id}`)
+        secElement.classList.add('active')
+    })
+})
+
+themeBtn.addEventListener('click', () => {
+    let body = document.body
+    body.classList.toggle('light-mode')
+})
